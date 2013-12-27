fc6df76f4771e82bba2a79b79acc486fdc559622
Leonel Tomes <mr.leonel.tomes@gmail.com>
2013-12-27 23:44:25 +0800

    Add application routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,30 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Incorporation Routes
+|--------------------------------------------------------------------------
+|
+| Register all the pages for the incorporation app.
+|
+*/
+Route::get('assumptions', array('uses' => 'AssumptionsController@index', 'as' => 'assumptions.index'));
+
+Route::get('options', array('uses' => 'OptionsController@edit', 'as' => 'options.edit'));
+Route::put('options', array('uses' => 'OptionsController@update', 'as' => 'options.update'));
+
+Route::get('data_entry', array('uses' => 'DataEntryController@edit', 'as' => 'data_entry.edit'));
+Route::put('data_entry', array('uses' => 'DataEntryController@update', 'as' => 'data_entry.update'));
+
+Route::group(array('prefix' => 'results'), function() {
+	Route::get('sole_trader', array('uses' => 'SoleTraderController@show', 'as' => 'results.sole_trader.show'));
+	Route::get('salary', array('uses' => 'SalaryController@show', 'as' => 'results.salary.show'));
+	Route::get('dividend', array('uses' => 'DividendController@show', 'as' => 'results.dividend.show'));
+});
+
+Route::get('summary', array('uses' => 'SummaryController@show', 'as' => 'summary.show'));
+Route::get('report', array('uses' => 'ReportController@download', 'as' => 'report.download'));
+
+Route::resource('clients', 'ClientController');
