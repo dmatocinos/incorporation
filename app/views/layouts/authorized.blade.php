2bac13e35059a5c3f103e449000f725a76d76ed6
Leonel Tomes <mr.leonel.tomes@gmail.com>
2013-12-29 11:02:53 +0800

    update links to asset using proper call as well as on routes

--- app/views/layouts/authorized.blade.php
+++ app/views/layouts/authorized.blade.php
@@ -8,15 +8,15 @@
 
 	<title>
 		@section('title') 
-		@show
+		@show | Incorporation Tax Planner
 	</title>
 
 	{{-- Bootstrap core CSS --}}
-	<link href="assets/css/bootstrap.css" rel="stylesheet">
+	<link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet">
 
 	{{-- Add custom CSS here --}}
-	<link href="assets/css/sb-admin.css" rel="stylesheet">
-	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+	<link href="{{ asset('assets/css/sb-admin.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}">
 
 	{{-- Additional Stylesheets --}}
 	@section('styles')
@@ -43,12 +43,15 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 				<ul class="nav navbar-nav side-nav">
-					<li class=""><a href="/data_entry"><i class="fa fa-edit"></i> Data Entry</a></li>
-					<li class=""><a href="/results"><i class="fa fa-bar-chart-o"></i> Results</a></li>
-					<li class=""><a href="/summary"><i class="fa fa-table"></i> Summary</a></li>
+					<li class=""><a href="{{ route('data_entry.edit') }}"><i class="fa fa-edit"></i> Data Entry</a></li>
+					<li class=""><a href="{{ route('results.salary.show') }}"><i class="fa fa-bar-chart-o"></i> Results</a></li>
+					<li class=""><a href="{{ route('summary.show') }}"><i class="fa fa-table"></i> Summary</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right navbar-user">
+					<li class="">
+						<a href="{{ route('options.edit') }}" class="" ><i class="fa fa-cog"></i> Options</a>
+					</li>
 					<li class="dropdown user-dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
 						<ul class="dropdown-menu">
@@ -66,18 +69,22 @@
 
 			<div class="row">
 				<div class="col-lg-12">
+					<div class="page-header">
+						<h1>
+							@section('title') 
+							@show
+						</h1>
+					</div>
 					{{-- Notifications --}}
-            <div class="alert alert-success alert-dismissable">
-              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-              Welcome to SB Admin by <a class="alert-link" href="http://startbootstrap.com">Start Bootstrap</a>! Feel free to use this template for your admin needs! We are using a few different plugins to handle the dynamic tables and charts, so make sure you check out the necessary documentation links provided.
-            </div>
 				</div>
 			</div><!-- /.row -->
 
 			<div class="row">
 				<div class="col-lg-12">
 					{{-- Content --}}
+					@section('content')
 					<h1>Blank Page <small>A Blank Slate</small></h1>
+					@show
 				</div>
 			</div><!-- /.row -->
 
@@ -86,8 +93,8 @@
 	</div><!-- /#wrapper -->
 
 	<!-- JavaScript -->
-	<script src="assets/js/jquery-1.10.2.js"></script>
-	<script src="assets/js/bootstrap.js"></script>
+	<script src="{{ asset('assets/js/jquery-1.10.2.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
 
 	@section('scripts')
 	@show
