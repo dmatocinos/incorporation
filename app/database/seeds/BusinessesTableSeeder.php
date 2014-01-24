f3290a9bc97e686dd2bdba6ba4da01f1075f5ace
Leonel Tomes <mr.leonel.tomes@gmail.com>
2014-01-24 14:54:44 +0800

    change 60000 to 50000

--- app/database/seeds/BusinessesTableSeeder.php
+++ app/database/seeds/BusinessesTableSeeder.php
@@ -10,7 +10,7 @@ class BusinessesTableSeeder extends Seeder {
 		$businesses = array(
 			'business_entity'	=> 'Sole Trader',
 			'net_profit_before_tax'	=> '100000',
-			'amount_to_distribute'	=> '60000'
+			'amount_to_distribute'	=> '50000'
 		);
 
 		// Uncomment the below to run the seeder
