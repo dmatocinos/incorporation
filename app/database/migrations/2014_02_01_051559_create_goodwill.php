910f9da56c82974bd4911496804c5245523e0fe0
Leonel Tomes <mr.leonel.tomes@gmail.com>
2014-02-03 16:24:42 +0800

    move has_goodwill to users table;

--- app/database/migrations/2014_02_01_051559_create_goodwill.php
+++ app/database/migrations/2014_02_01_051559_create_goodwill.php
@@ -11,12 +11,16 @@ class CreateGoodwill extends Migration {
 	 */
 	public function up()
 	{
-		Schema::table('business', function($table)
+		Schema::table('businesses', function($table)
 		{
-			$this->integer('has_goodwill');
-			$this->integer('goodwill_estimated_value');
-			$this->string('existing_business_commenced');
-			$this->integer('goodwill_write_off_years');
+			$table->integer('goodwill_estimated_value');
+			$table->string('existing_business_commenced');
+			$table->integer('goodwill_write_off_years');
+		});
+
+		Schema::table('users', function($table)
+		{
+			$table->integer('has_goodwill');
 		});
 	}
 
