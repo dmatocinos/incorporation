294c669a5ee62f488b9b1d7bf6a3cd4edecb5e3f
Dixie Philamerah J. Atay <dixie.atay@gmail.com>
2014-02-06 09:33:50 +0800

    Move has_goodwill to businesses table

--- app/database/migrations/2014_02_01_051559_create_goodwill.php
+++ app/database/migrations/2014_02_01_051559_create_goodwill.php
@@ -16,10 +16,6 @@ class CreateGoodwill extends Migration {
 			$table->integer('goodwill_estimated_value');
 			$table->string('existing_business_commenced');
 			$table->integer('goodwill_write_off_years');
-		});
-
-		Schema::table('users', function($table)
-		{
 			$table->integer('has_goodwill');
 		});
 	}
