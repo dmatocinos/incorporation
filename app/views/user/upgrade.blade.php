d57c1d6439e5f588931dcaacad87387a6766fb12
Dixie Philamerah J. Atay <dixie.atay@gmail.com>
2014-05-04 21:58:59 +0800

    Updated upgrade buttons

--- app/views/user/upgrade.blade.php
+++ app/views/user/upgrade.blade.php
@@ -7,17 +7,17 @@
 	  <label>{{ $msg }} </label>
 	</div>
 	<div class="subscribe" style="margin: 30px 0px 0px; text-align: center; width: 100%; float: left;">
-		<a style="font-size: 18px; float: center;" class="text-center col-xs-6 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/third') }}"> Upgrade to <b>Professional </b> &nbsp; | 41 downloads</a>
+		<a style="font-size: 18px; float: center;" class="text-center col-xs-9 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/third') }}">Upgrade to <b>Professional</b> for only £499.00 and have up to 41 downloads</a>
 	</div>
 	@elseif ($level == 'Pay As You Go') 
 	<div class="form-group">
 	  <label>{{ $msg }} </label>
 	</div>
 	<div class="subscribe" style="margin: 30px 0px 0px; text-align: center; width: 100%; float: left;">
-		<a style="font-size: 18px; float: center;" class="text-center col-xs-6 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/second') }}"> Upgrade to <b>Pro Active </b> &nbsp; | 11 downloads</a>
+		<a style="font-size: 18px; float: center;" class="text-center col-xs-9 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/second') }}">Upgrade to <b>Pro Active</b> for only £99.0 and have up to 11 downloads</a>
 	</div>
 	<div class="subscribe" style="margin: 30px 0px 0px; text-align: center; width: 100%; float: left;">
-		<a style="font-size: 18px; float: center;" class="text-center col-xs-6 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/third') }}"> Upgrade to <b>Professional </b> &nbsp; | 41 downloads</a>
+		<a style="font-size: 18px; float: center;" class="text-center col-xs-9 btn btn-success logo-color-bg" href="{{ url('upgrade/start_payment/third') }}">Upgrade to <b>Professional</b> for only £499.00 and have up to 41 downloads</a>
 	</div>
 	@endif
 	<div class="subscribe" style="margin: 20px 0px 30px; text-align: center; width: 100%; float: left;">
